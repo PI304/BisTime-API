42713a1c009c9df8aaf8952e237390692fc8bdd7
linda2927 <linda2927@naver.com>
2022-12-02 17:00:32 +0900

    feat: add configuration files for test deploy

---
+++ config/settings/deploy.py
@@ -0,0 +1,8 @@
+from .base import *
+
+# DEBUG = False
+ALLOWED_HOSTS = [".ap-northeast-2.compute.amazonaws.com"]  # 인스턴스 IPv4 기반 퍼블릭 DNS 주소 or 퍼블릭 IP
+WSGI_APPLICATION = "config.wsgi.deploy.application"
+
+STATIC_ROOT = BASE_DIR / 'static/'
+STATICFILES_DIRS = []
