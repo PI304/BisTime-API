b927c6e8c7226a76d63451c79e20f744c6435d99
linda2927 <linda2927@naver.com>
2022-12-02 21:49:35 +0900

    fix: edit configurations for gunicorn and nginx

--- infra/scripts/configure.sh
+++ infra/scripts/configure.sh
@@ -4,3 +4,4 @@ cp nginx/nginx.conf /etc/nginx
 cp -r nginx/conf.d /etc/nginx
 cp systemd/gunicorn.socket /etc/systemd/system
 cp systemd/gunicorn.service /etc/systemd/system
+cd /home/ubuntu/BisTime-API
