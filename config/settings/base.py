61c89c13e2685a713c68a354575eb9e25b7fd5e8
linda2927 <linda2927@naver.com>
2022-12-03 22:18:45 +0900

    fix: add url for static files

--- config/settings/base.py
+++ config/settings/base.py
@@ -172,8 +172,11 @@ TIME_ZONE = "Asia/Seoul"
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-
-# Default primary key field type
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+    # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
