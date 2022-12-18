bffb6c3fcb6fe20897e68dd10cd8451a728b74a8
linda2927 <linda2927@naver.com>
2022-12-19 01:02:09 +0900

    fix: use plurals for url

--- apps/event/urls.py
+++ apps/event/urls.py
@@ -9,12 +9,12 @@ from apps.event.views import (
 )
 
 urlpatterns = [
-    path("", EventView.as_view(), name="create-instant-event"),
-    path("<int:pk>/", EventUpdateView.as_view(), name="update-instant-event"),
-    path("<int:pk>/date/", EventDateView.as_view(), name="event-dates"),
-    path("<int:pk>/schedule/", ScheduleList.as_view(), name="schedule-list"),
+    path("", EventView.as_view(), name="event-list"),
+    path("<int:pk>/", EventUpdateView.as_view(), name="event-detail"),
+    path("<int:pk>/dates/", EventDateView.as_view(), name="event-dates"),
+    path("<int:pk>/schedules/", ScheduleList.as_view(), name="schedule-list"),
     path(
-        "<int:pk>/schedule/<str:name>/",
+        "<int:pk>/schedules/<str:name>/",
         ScheduleDetail.as_view(),
         name="schedule-detail",
     ),
