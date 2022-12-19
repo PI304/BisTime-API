75e21ef8ca576a389e2ad7ade84936630c38001f
linda2927 <linda2927@naver.com>
2022-12-20 00:05:05 +0900

    feat: reconfigure url path for Schedules

--- apps/event/urls.py
+++ apps/event/urls.py
@@ -4,18 +4,24 @@ from apps.event.views import (
     EventView,
     EventUpdateView,
     EventDateView,
-    ScheduleList,
-    ScheduleDetail,
+    ScheduleView,
+    UserScheduleView,
+    BulkCreateUserScheduleView,
 )
 
 urlpatterns = [
     path("", EventView.as_view(), name="event-list"),
     path("<int:pk>/", EventUpdateView.as_view(), name="event-detail"),
     path("<int:pk>/dates/", EventDateView.as_view(), name="event-dates"),
-    path("<int:pk>/schedules/", ScheduleList.as_view(), name="schedule-list"),
+    path("<int:pk>/schedules/", ScheduleView.as_view(), name="schedule-list"),
     path(
         "<int:pk>/schedules/<str:name>/",
-        ScheduleDetail.as_view(),
-        name="schedule-detail",
+        UserScheduleView.as_view(),
+        name="user-schedule-list",
+    ),
+    path(
+        "<int:pk>/schedules/<str:name>/bulk",
+        BulkCreateUserScheduleView.as_view(),
+        name="bulk-create-user-schedule",
     ),
 ]
