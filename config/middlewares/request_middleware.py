e4da0263cf6705c5468f567ce55f285ffa3197f7
linda2927 <linda2927@naver.com>
2023-01-04 15:58:41 +0900

    fix: check if Accept header is appropriately configured

--- config/middlewares/request_middleware.py
+++ config/middlewares/request_middleware.py
@@ -32,12 +32,10 @@ class RequestMiddleware:
         if "api" not in request.path:
             raise ValidationError("all urls must include '/api'")
         else:
-            if len(request.headers["Accept"].split(";")) < 2:
-                raise ValidationError(
-                    "Accept header must be 'application/json; version=1;'"
-                )
-            if (
-                request.path.split("/")[2] not in ["swagger", "redoc", "admin"]
-                and "version" not in request.headers["Accept"].split(";")[1]
-            ):
-                raise ValidationError("Accept header must include api version")
+            if request.path.split("/")[2] not in ["swagger", "redoc", "admin"]:
+                if len(request.headers["Accept"].split(";")) < 2:
+                    raise ValidationError(
+                        "Accept header must be 'application/json; version=1;'"
+                    )
+                if "version" not in request.headers["Accept"].split(";")[1]:
+                    raise ValidationError("Accept header must include api version")
