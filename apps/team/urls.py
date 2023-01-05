d37046067211f9b83ac2d602d0be02f250dd0cf5
linda2927 <linda2927@naver.com>
2023-01-05 13:25:39 +0900

    feat: add url patterns for team endpoint

--- apps/team/urls.py
+++ apps/team/urls.py
@@ -1,3 +1,28 @@
 from django.urls import path, include
+from .views import (
+    TeamCreateView,
+    TeamDetailView,
+    TeamRegularEventListView,
+    TeamRegularEventDetailView,
+    SubgroupListView,
+    SubgroupDetailView,
+)
 
-urlpatterns = []
+urlpatterns = [
+    path("", TeamCreateView.as_view(), name="team-create"),
+    path("<int:pk>", TeamDetailView.as_view(), name="team-detail"),
+    path(
+        "<int:pk>/events/", TeamRegularEventListView.as_view(), name="team-events-list"
+    ),
+    path(
+        "<int:pk>/events/<int:event_id>",
+        TeamRegularEventDetailView.as_view(),
+        name="team-events-detail",
+    ),
+    path("<int:pk>/subgroups/", SubgroupListView.as_view(), name="subgroups-create"),
+    path(
+        "<int:pk>/subgroups/<int:subgroup_id>/",
+        SubgroupDetailView.as_view(),
+        name="subgroups-create",
+    ),
+]
