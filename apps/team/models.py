86c71ff551a9616ddb14f5c464acead7abda9a6b
linda2927 <linda2927@naver.com>
2023-01-08 13:20:04 +0900

    feat: add uuid field to Team model and description field for TeamRegularEvent model

--- apps/team/models.py
+++ apps/team/models.py
@@ -11,13 +11,20 @@ class Team(TimeStampMixin):
     """
 
     id = models.BigAutoField(primary_key=True)
+    uuid = models.CharField(
+        max_length=23,
+        validators=[MinLengthValidator(23)],
+        null=False,
+        help_text="팀을 구분하거나, 팀 뷰 url 생성을 위한 uuid 문자열",
+    )
     name = models.CharField(max_length=100, null=False)
     admin_code = models.CharField(
         max_length=6, validators=[MinLengthValidator(6)], null=False
     )
     security_question = models.ForeignKey(
-        SecurityQuestion, on_delete=models.DO_NOTHING, related_name="team"
+        SecurityQuestion, on_delete=models.DO_NOTHING, null=True
     )
+    custom_security_question = models.CharField(max_length=200, null=True)
     security_answer = models.CharField(max_length=50, null=False)
 
     class Meta:
@@ -30,7 +37,7 @@ class Team(TimeStampMixin):
         return f"Team({self.id}, {self.name})"
 
 
-class TeamEvent(TimeStampMixin, TimeBlockMixin):
+class TeamRegularEvent(TimeStampMixin, TimeBlockMixin):
     """
     Regular team events
     """
@@ -46,13 +53,21 @@ class TeamEvent(TimeStampMixin, TimeBlockMixin):
 
     id = models.BigAutoField(primary_key=True)
     uuid = models.CharField(
-        max_length=22, validators=[MinLengthValidator(22)], null=False
+        max_length=23, validators=[MinLengthValidator(23)], null=False
     )
+    title = models.CharField(max_length=100, null=False, blank=False)
+    description = models.CharField(max_length=200, null=False, blank=True)
     team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="team_event")
     day = models.IntegerField(choices=DayOfWeek.choices, null=False)
 
     def get_day_of_week(self, day_of_week: str) -> DayOfWeek:
         return self.DayOfWeek[day_of_week]
+
+    def __str__(self) -> str:
+        return f"[{self.id}] {self.title} (team_id: {self.team_id}"
+
+    def __repr__(self) -> str:
+        return f"RegularEvent({self.id}, {self.title})"
 
 
 class SubGroup(TimeStampMixin):
@@ -66,9 +81,10 @@ class SubGroup(TimeStampMixin):
 
     class Meta:
         db_table = "subgroup"
+        unique_together = [["name", "team_id"]]
 
     def __str__(self) -> str:
-        return f"[{self.id}] Team: {self.team}, {self.name}"
+        return f"[{self.id}] {self.name} (team_id: {self.team_id}"
 
     def __repr__(self) -> str:
         return f"SubGroup({self.id}, {self.team}, {self.name})"
