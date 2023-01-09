80b4c577a240b6b6b024b22af8905a7c34a2fbb2
linda2927 <linda2927@naver.com>
2023-01-09 15:30:55 +0900

    feat: add endpoints for verifying and resetting admin code

--- apps/team/views.py
+++ apps/team/views.py
@@ -7,7 +7,7 @@ from django.utils.decorators import method_decorator
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import generics, status
-from rest_framework.exceptions import PermissionDenied, ValidationError
+from rest_framework.exceptions import PermissionDenied, AuthenticationFailed
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -317,28 +317,53 @@ class SubgroupDetailView(generics.RetrieveUpdateDestroyAPIView):
 class TeamAdminCodeVerificationView(APIView):
     @swagger_auto_schema(
         operation_summary="Check admin code",
-        operation_description="Place admin code input value in 'Authorization' header using 'Basic' scheme",
         responses={
-            201: "No content",
+            204: "No content",
             404: "Team with the provided uuid does not exist",
         },
     )
-    def get(self, request: Request, *args: Any, **kwargs: Any) -> Response:
-        if (
-            request.headers["Authorization"]
-            and len(request.headers["Authorization"].split(" ")) == 2
-        ):
-            admin_code = request.headers["Authorization"].split(" ")[1]
-            try:
-                instance = get_object_or_404(Team, uuid=kwargs.get("uuid"))
-            except Http404:
-                raise InstanceNotFound("Team with the provided uuid does not exist")
-
-            if instance.admin_code != admin_code:
-                raise PermissionDenied("Wrong admin code")
-            else:
-                return Response(status=status.HTTP_204_NO_CONTENT)
+    def post(self, request: Request, *args: Any, **kwargs: Any) -> Response:
+        admin_code = request.data.get("admin_code")
+        try:
+            instance = get_object_or_404(Team, uuid=kwargs.get("uuid"))
+        except Http404:
+            raise InstanceNotFound("Team with the provided uuid does not exist")
+        if instance.admin_code != admin_code:
+            raise PermissionDenied("Wrong admin code")
         else:
-            raise ValidationError(
-                "Wrongly configured Authorization header. Use 'Basic' scheme."
-            )
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class TeamAdminCodeResetView(APIView):
+    @swagger_auto_schema(
+        operation_summary="Reset admin code",
+        operation_description="Returns randomly generated new admin code",
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            required=["securityAnswer"],
+            properties={
+                "securityAnswer": openapi.Schema(
+                    type=openapi.TYPE_STRING, description="보안 질문 정답"
+                )
+            },
+        ),
+        responses={
+            200: openapi.Response("Success", TeamSerializer),
+            404: "Team with the provided uuid does not exist",
+        },
+    )
+    def post(self, request: Request, *args: Any, **kwargs: Any) -> Response:
+        security_answer = request.data.get("security_answer")
+        try:
+            instance = get_object_or_404(Team, uuid=kwargs.get("uuid"))
+        except Http404:
+            raise InstanceNotFound("Team with the provided uuid does not exist")
+
+        if instance.security_answer != security_answer:
+            raise AuthenticationFailed("Wrong answer for security question")
+        else:
+            instance.admin_code = TeamService.generate_admin_code()
+            instance.updated_at = datetime.now()
+            instance.save(update_fields=["admin_code", "updated_at"])
+
+            return Response(TeamSerializer(instance).data, status=status.HTTP_200_OK)
