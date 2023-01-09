c164355e1439c665109331beaefdbdd28ffc7db3
linda2927 <linda2927@naver.com>
2023-01-10 07:38:43 +0900

    feat: make a separate directory for views

---
+++ apps/team/views/fixed_schedule_views.py
@@ -0,0 +1,9 @@
+from rest_framework import generics
+
+
+class TeamMemberFixedScheduleListView(generics.ListCreateAPIView):
+    pass
+
+
+class TeamMemberFixedScheduleDetailView(generics.RetrieveUpdateDestroyAPIView):
+    pass
