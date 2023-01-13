699bc35df5f3760134e6089bce75ecbb16fe9aec
linda2927 <linda2927@naver.com>
2023-01-13 15:30:09 +0900

    feat: add TeamMember serializer and WeekSchedule serializer for saving members' schedule in both rds and s3

--- apps/team/serializers.py
+++ apps/team/serializers.py
@@ -6,8 +6,8 @@ from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
 from apps.event.serializers import EventSerializer
-from apps.team.models import Team, TeamRegularEvent, SubGroup
-from apps.team.services import TeamMemberFixedScheduleService
+from apps.team.models import Team, TeamRegularEvent, SubGroup, TeamMember
+from apps.team.services import TeamMemberService
 from config.exceptions import InstanceNotFound
 
 
@@ -104,40 +104,100 @@ class SubgroupSerializer(serializers.ModelSerializer):
         read_only_fields = ["id", "team", "created_at", "updated_at"]
 
 
-class TeamMemberFixedScheduleSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=20, min_length=1)
-    subgroup = serializers.CharField(max_length=50, min_length=1)
+class TeamUUIDField(serializers.Field):
+    def to_representation(self, value: Team) -> str:
+        team = get_object_or_404(Team, id=value.id)
+        return team.uuid
+
+    def to_internal_value(self, data):
+        print(data)
+        if not isinstance(data, str):
+            msg = "Incorrect type. Expected a str (uuid), but got %s"
+            raise ValidationError(msg % type(data).__name__)
+        try:
+            team = get_object_or_404(Team, uuid=data)
+        except Http404:
+            raise InstanceNotFound("team with the provided uuid does not exist")
+        return team.id
+
+
+class TeamSubgroupField(serializers.Field):
+    def to_representation(self, value: SubGroup) -> str:
+        subgroup = get_object_or_404(SubGroup, id=value.id)
+        return subgroup.name
+
+    def to_internal_value(self, data):
+        if not isinstance(data, str):
+            msg = "Incorrect type. Expected a str (name), but got %s"
+            raise ValidationError(msg % type(data).__name__)
+        try:
+            subgroup = get_object_or_404(SubGroup, name=data)
+        except Http404:
+            raise InstanceNotFound("subgroup with the provided id does not exist")
+        return subgroup.id
+
+
+class TeamMemberSerializer(serializers.ModelSerializer):
+    team = TeamUUIDField()
+    subgroup = TeamSubgroupField()
+
+    class Meta:
+        model = TeamMember
+        fields = ["id", "name", "team", "subgroup", "created_at", "updated_at"]
+        read_only_fields = ["id", "subgroup", "team", "created_at", "updated_at"]
+
+    def create(self, validated_data):
+        print(validated_data)
+        return TeamMember.objects.create(
+            team_id=validated_data["team"],
+            subgroup_id=validated_data["subgroup"],
+            name=validated_data["name"],
+        )
+
+    def update(self, instance, validated_data):
+        instance.subgroup_id = validated_data["subgroup"]
+        instance.updated_at = validated_data["updated_at"]
+        instance.save(update_fields=["subgroup", "updated_at"])
+        return instance
+
+
+class WeekScheduleSerializer(serializers.Serializer):
+    team = TeamUUIDField()
+    name = serializers.CharField(max_length=20)
     week_schedule = serializers.ListField(min_length=7, max_length=7)
 
-    def validate_subgroup(self, value):
+    def validate_week_schedule(self, value):
+        for s in value:
+            if not isinstance(s, str):
+                raise ValidationError("list items should be of string type")
+            if len(s) != 48:
+                raise ValidationError("each items should be 48-length long")
+        return value
+
+    def validate_team(self, value):
         try:
-            subgroup = get_object_or_404(SubGroup, name=value)
+            team = get_object_or_404(Team, id=value)
         except Http404:
-            raise InstanceNotFound("subgroup with the provided name does not exist")
+            raise InstanceNotFound("team with the provided uuid does not exist")
 
         return value
 
-    def validate_schedule(self, value):
-        for x in value:
-            if type(x) is not str:
-                raise ValidationError("schedule should contain string format children")
-
-        return value
-
-    def save(self, team_name: str):
+    def save(self):
         schedule_bytes: List[bytearray] = []
 
-        for bytes_string in self.validated_data["schedule"]:
+        try:
+            team = get_object_or_404(Team, id=self.validated_data["team"])
+        except Http404:
+            raise InstanceNotFound("team with the provided uuid does not exist")
+
+        for bytes_string in self.validated_data["week_schedule"]:
             schedule_bytes.append(bytearray([int(x) for x in bytes_string]))
 
-        print(schedule_bytes)
-
-        bitmap_bytes: bytes = (
-            TeamMemberFixedScheduleService.create_schedule_bitmap_bytes(schedule_bytes)
+        bitmap_bytes: bytes = TeamMemberService.create_schedule_bitmap_bytes(
+            schedule_bytes
         )
-        TeamMemberFixedScheduleService.save_schedule(
+        TeamMemberService.save_schedule(
             bitmap_bytes,
-            team_name,
+            team.name,
             self.validated_data["name"],
-            self.validated_data["subgroup"],
         )
