c4c51680193da37981887b3defb1d9f6aac513f8
linda2927 <linda2927@naver.com>
2023-01-14 11:54:17 +0900

    feat: add method for getting s3 resource instance

--- config/s3_config.py
+++ config/s3_config.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-def s3_connection():
+def s3_client_connection():
     try:
         # s3 클라이언트 생성
         s3 = boto3.client(
@@ -20,3 +20,13 @@ def s3_connection():
     else:
         print("s3 bucket connected!")
         return s3
+
+
+def s3_bucket():
+    s3 = boto3.resource(
+        "s3",
+        aws_access_key_id=os.environ.get("S3_USER_ACCESS_KEY"),
+        aws_secret_access_key=os.environ.get("S3_USER_SECRET_KEY"),
+    )
+    bucket = s3.Bucket(os.environ.get("S3_BUCKET_NAME"))
+    return bucket
