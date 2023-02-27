411e3641d88e97f24c013e14fd86dfb83c6bdd37
linda2927 <linda2927@naver.com>
2023-02-27 23:07:08 +0900

    deploy: update ssl renewal script

--- infra/scripts/ssl_renewal.sh
+++ infra/scripts/ssl_renewal.sh
@@ -1,5 +1,5 @@
-docker-compose stop bistime-nginx
+docker-compose stop
 sudo certbot renew
 cd /home/ubuntu/BisTime-API
 . scripts/copy_ssl_files.sh
-docker-compose restart bistime-nginx
+docker-compose restart
