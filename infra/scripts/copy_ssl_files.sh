ae29d9dddafcfc8e379069cd4b28b862496d2886
linda2927 <linda2927@naver.com>
2023-02-27 22:14:19 +0900

    deploy: configurations for docker

--- infra/scripts/copy_ssl_files.sh
+++ infra/scripts/copy_ssl_files.sh
@@ -2,7 +2,7 @@ cd /home/ubuntu/BisTime-API
 sudo rm -r /certs
 mkdir certs
 cd certs
-sudo cp /etc/letsencrypt/live/bistime.app/fullchain.pem /home/ubuntu/BisTinme-API/certs/
-sudo cp /etc/letsencrypt/live/bistime.app/privkey.pem /home/ubuntu/BisTinme-API/certs/
-sudo cp /etc/letsencrypt/options-ssl-nginx.conf /home/ubuntu/BisTinme-API/certs/
-sudo cp /etc/letsencrypt/ssl-dhparams.pem /home/ubuntu/BisTinme-API/certs/
+sudo cp /etc/letsencrypt/live/bistime.app/fullchain.pem /home/ubuntu/BisTime-API/certs/
+sudo cp /etc/letsencrypt/live/bistime.app/privkey.pem /home/ubuntu/BisTime-API/certs/
+sudo cp /etc/letsencrypt/options-ssl-nginx.conf /home/ubuntu/BisTime-API/certs/
+sudo cp /etc/letsencrypt/ssl-dhparams.pem /home/ubuntu/BisTime-API/certs/
