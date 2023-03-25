963727b78637c983c0fc717a69639155835dca31
linda2927 <linda2927@naver.com>
2023-03-25 11:54:59 +0900

    fix: use certbot container for ssl renewal

--- infra/scripts/ssl_renewal.sh
+++ infra/scripts/ssl_renewal.sh
@@ -1,5 +1,10 @@
-docker-compose stop
-sudo certbot renew
-cd /home/ubuntu/BisTime-API
-. scripts/copy_ssl_files.sh
-docker-compose restart
+#!/bin/bash
+
+docker run --rm --name certbot \
+-v '/home/ubuntu/BisTime-API/certbot/conf:/etc/letsencrypt' \
+-v '/home/ubuntu/BisTime-API/certbot/logs:/var/log/letsencrypt' \
+-v '/home/ubuntu/BisTime-API/certbot/data:/var/www/letsencrypt' \
+certbot/certbot certonly --webroot -w /var/www/letsencrypt --force-renewal --server https://acme-v02.api.letsencrypt.org/directory \
+--cert-name api.bistime.app
+
+docker exec bistime-nginx nginx -s reload
