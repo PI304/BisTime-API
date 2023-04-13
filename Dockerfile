a6177830bc3572df72ee44389f0757a7ec756ae6
linda2927 <linda2927@naver.com>
2023-04-13 16:22:41 +0900

    deploy: add entrypoint.sh for gunicorn worker

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM python:3.10
 
-COPY ./ /home/convey/
+COPY ./ /home/bistime/
 
-WORKDIR /home/convey/
+WORKDIR /home/bistime/
 
 RUN apt-get update
 
@@ -10,4 +10,8 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 EXPOSE 8080
 
-CMD ["bash", "-c", "python3 manage.py migrate && gunicorn config.wsgi.deploy:application --bind 0.0.0.0:8080 --timeout=30"]
+COPY ./entrypoint.sh /
+RUN chmod 747 /entrypoint.sh
+ENTRYPOINT ["/entrypoint.sh"]
+
+#CMD ["bash", "-c", "python3 manage.py migrate && gunicorn config.wsgi.deploy:application --bind 0.0.0.0:8080 --timeout=30"]
---
+++ entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Prepare log files and start outputting logs to stdout
+mkdir -p logs
+cd logs
+touch gunicorn.log
+touch gunicorn-access.log
+touch gunicorn-error.log
+tail -n 0 -f gunicorn*.log &
+
+export DJANGO_SETTINGS_MODULE=config.settings.deploy
+
+cd /home/bistime
+python3 manage.py migrate || exit 1
+
+exec gunicorn config.wsgi.deploy:application \
+    --name bistime \
+    --bind 0.0.0.0:8080 \
+    --workers 1 \
+    --log-level=debug \
+    --log-file=./logs/gunicorn.log \
+    --access-logfile=./logs/gunicorn-access.log \
+    --error-logfile=./logs/gunicorn-error.log \
+"$@"
